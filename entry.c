65f5179d4769346c37aa74909e3a53094c8875d0
Saeed Noursalehi <sanoursa@microsoft.com>
2016-06-03 15:16:22 +0000

    PR 111856: TEMPORARY ugly hack to avoid deleting files before writing updated version
    Calling unlink before updating a file causes problems with GVFS until we have proper tombstones for deleted files.  TEMPORARILY remove the unlink call until we have the right, long term fix.  The work to undo this hack is being tracked here: https://mseng.visualstudio.com/VSOnline/_workitems/edit/597008

--- entry.c
+++ entry.c
@@ -76,7 +76,16 @@ static void remove_subtree(struct strbuf *path)
 static int create_file(const char *path, unsigned int mode)
 {
 	mode = (mode & 0100) ? 0777 : 0666;
+	/*
+	* BUG BUG: Calling open here with O_EXCL causes problems with GVFS 
+	* as we can't delete the file until we have proper tombstones for deleted files.  
+	* TEMPORARILY remove this flag until we have the right, long term fix.
+	*
+	* https://mseng.visualstudio.com/VSOnline/_workitems/edit/597008
+	*
 	return open(path, O_WRONLY | O_CREAT | O_EXCL, mode);
+	*/
+	return open(path, O_WRONLY | O_CREAT | O_TRUNC, mode);
 }
 
 static void *read_blob_entry(const struct cache_entry *ce, unsigned long *size)
@@ -283,9 +292,18 @@ int checkout_entry(struct cache_entry *ce,
 			if (!state->force)
 				return error("%s is a directory", path.buf);
 			remove_subtree(&path);
-		} else if (unlink(path.buf))
+		}
+		/*
+		 * BUG BUG: Calling unlink here causes problems with GVFS until
+		 * we have proper tombstones for deleted files.  TEMPORARILY
+		 * remove this call until we have the right, long term fix.
+		 *
+		 * https://mseng.visualstudio.com/VSOnline/_workitems/edit/597008 
+		 *
+		  else if (unlink(path.buf))
 			return error("unable to unlink old '%s' (%s)",
 				     path.buf, strerror(errno));
+		 */
 	} else if (state->not_new)
 		return 0;
 
