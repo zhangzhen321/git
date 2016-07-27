caf306bf9edfaf294c2f9587627f1922b7559857
Saeed Noursalehi <sanoursa@microsoft.com>
2016-07-27 14:04:15 -0700

    Updated installer version number

--- VSTSBuild/buildinstaller.sh
+++ VSTSBuild/buildinstaller.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 USERPROFILE=/c/GitBuildOutput
 export USERPROFILE
-/usr/src/build-extra/installer/release.sh 2.9.0-gvfs
+/usr/src/build-extra/installer/release.sh 2.9.2-gvfs
 
 HOME=/c/GitBuildOutput
 export HOME
-/usr/src/build-extra/portable/release.sh 2.9.0-gvfs
+/usr/src/build-extra/portable/release.sh 2.9.2-gvfs
