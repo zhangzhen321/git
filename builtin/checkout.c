9a573ef19f5b797285ab42b7aa36f88fea304b90
Ben Peart <Ben.Peart@microsoft.com>
2016-10-21 23:01:09 +0000

    PR 153303: Don't show local changes after doing an optimized "checkout -b foo"
    Don't show local changes on the optimized "checkout -b" path as the optimized path does not compute the set of changes so can't report them correctly.  Since this path doesn't make any changes (other than creating a new reference and pointing head to it) the results of a "git status" before and after will be identical.
    
    Related work items: #682832

--- builtin/checkout.c
+++ builtin/checkout.c
@@ -683,10 +683,10 @@ static int merge_working_tree(const struct checkout_opts *opts,
 
 		if (write_locked_index(&the_index, lock_file, COMMIT_LOCK))
 			die(_("unable to write new index file"));
-	}
 
-	if (!opts->force && !opts->quiet)
-		show_local_changes(&new->commit->object, &opts->diff_options);
+		if (!opts->force && !opts->quiet)
+			show_local_changes(&new->commit->object, &opts->diff_options);
+	}
 
 	return 0;
 }
