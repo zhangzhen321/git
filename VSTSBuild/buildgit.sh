a74b75767496411e2679881227e7fc75895908c4
Saeed Noursalehi <sanoursa@microsoft.com>
2017-01-10 14:31:03 -0800

    Delete unused scripts

--- VSTSBuild/buildgit.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-cd /usr/src/git
-make -j15 install
--- VSTSBuild/buildinstaller.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-rm -f /tmp.7z
-USERPROFILE=/c/GitBuildOutput
-export USERPROFILE
-/usr/src/build-extra/installer/release.sh 2.10.2-gvfs
-
-HOME=/c/GitBuildOutput
-export HOME
-/usr/src/build-extra/portable/release.sh 2.10.2-gvfs
