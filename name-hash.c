dbed8d40a5bfc682d1058e7db424f5dab6d53f8b
Jeff Hostetler <jeffhost@microsoft.com>
2017-03-16 14:03:42 -0400

    Revert "name-hash: specify initial size for istate.dir_hash table"
    This reverts commit 29a29767a45f5182b9c60e36b6e4ce803d33d640.

--- name-hash.c
+++ name-hash.c
@@ -150,8 +150,7 @@ static void lazy_init_name_hash(struct index_state *istate)
 	start = getnanotime();
 	hashmap_init(&istate->name_hash, (hashmap_cmp_fn) cache_entry_cmp,
 			istate->cache_nr);
-	hashmap_init(&istate->dir_hash, (hashmap_cmp_fn) dir_entry_cmp,
-			istate->cache_nr);
+	hashmap_init(&istate->dir_hash, (hashmap_cmp_fn) dir_entry_cmp, 0);
 	for (nr = 0; nr < istate->cache_nr; nr++)
 		hash_index_entry(istate, istate->cache[nr]);
 	istate->name_hash_initialized = 1;
